5fee787dbafd4576e63c64e3761284fbee865da8
yosef-girma <37630081+yosef-girma@users.noreply.github.com>
2023-06-09 05:38:02 +0300

    Update ajax_handler.js

--- xpr/server_js/ajax_handler.js
+++ xpr/server_js/ajax_handler.js
@@ -1,5 +1,11 @@
+var ajaxHandler = require('bundle-test/server_js/ajax_handler.js');
 
 exports.process = function(context, options) {
-
-    return "ajaxHandler from bundle test 2 updated content via webhook";
+        var x = ajaxHandler.process();
+    return x+"ajaxHandler from bundle test 2 updated content via webhook";
 }
+
+
+
+
+
